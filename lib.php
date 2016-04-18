685bd6be5ce9d33ce94406a3ed8d18d7ffd8a39d
Vadim Dvorovenko <Vadimon@mail.ru>
2016-04-18 10:28:03 +0800

    Navigation errors in other module fix

--- lib.php
+++ lib.php
@@ -77,6 +77,7 @@ function local_purgequestioncategory_extend_settings_navigation(navigation_node 
         return;
     }
     $parentnode = $nav->get('modulesettings');
+    $questionbank = null;
     foreach ($parentnode->children as $node) {
         if ($node->text == get_string('questionbank', 'question')) {
             $questionbank = $node;
